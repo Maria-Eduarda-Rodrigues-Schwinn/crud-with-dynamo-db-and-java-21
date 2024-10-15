ca8a720df2f2c50f395ce93072339381dd748660
maria-eduarda-schwinn <maria.eduarda.schwinn@gmail.com>
2024-10-15 12:37:06 -0300

    first commit

---
+++ src/test/java/tech/buildRun/dynamoDb/DynamondbApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildRun.dynamoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamondbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
